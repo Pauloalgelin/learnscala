627a12d940b3660c73a6dfb13ab6c641c06ab4eb
Pauloalgelin <pauloalgelin@gmail.com>
2018-12-17 17:12:35 -0200

    Function literal and function value.

--- closuresandfunctions/FuncClos.scala
+++ closuresandfunctions/FuncClos.scala
@@ -17,8 +17,24 @@ object LongLines {
 
 object FuncClos {
     def main(args: Array[String]): Unit = {
-        val width = args(0).toInt
-        for (arg <- args.drop(1))
-            LongLines.processFile(arg, width)
+        // val width = args(0).toInt
+        // for (arg <- args.drop(1))
+        //     LongLines.processFile(arg, width)
+
+        var increase = (x: Int) => x + 1
+        val num2 = increase(1)
+        println("increase(1): " + num2)
+
+        increase = (x: Int) => x + 3
+        val num3 = increase(1)
+        println("increase(1): " + num3)
+
+        val increasehello = (x: Int) => {
+            println("Hello,")
+            println("world!")
+            x + 1
+        }
+        val num4 = increasehello(4)
+        println("increasehello(4): " + num4)
     }
 }
