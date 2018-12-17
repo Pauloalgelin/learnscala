effddcc57eae4f7ae1516aab48a50b3f4aa616d6
Pauloalgelin <pauloalgelin@gmail.com>
2018-12-17 03:35:21 -0200

    Local functions. Helper nested functions.

--- closuresandfunctions/FuncClos.scala
+++ closuresandfunctions/FuncClos.scala
@@ -4,14 +4,15 @@ object LongLines {
     def processFile(filename: String, width: Int) = {
         val source = Source.fromFile(filename)
         for (line <- source.getLines())
-            processLine(filename, width, line)
-        //source.getLines().foreach(line => processLine(filename, width, line))
+            processLine(line)
+
+        def processLine(line: String) = {
+            if (line.length > width)
+                println(filename + ": " + line.trim)
+        }
+
     }
 
-    private def processLine(filename: String, width: Int, line: String) = {
-        if (line.length > width)
-            println(filename + ": " + line.trim)
-    }
 }
 
 object FuncClos {
