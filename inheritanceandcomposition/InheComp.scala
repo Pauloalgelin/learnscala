97e663c56bf9c90dd59ada8d3a946a0d53d34924
Pauloalgelin <pauloalgelin@gmail.com>
2018-12-20 20:15:08 -0200

    Extending classes. Inheritance, subtyping and composition.

--- inheritanceandcomposition/InheComp.scala
+++ inheritanceandcomposition/InheComp.scala
@@ -4,8 +4,20 @@ abstract class Element {
     def width: Int = if (height == 0) 0 else contents(0).length
 }
 
+class ArrayElement(conts: Array[String]) extends Element {
+    def contents: Array[String] = conts
+}
+
 object Element {
     def main(args: Array[String]): Unit = {
-        println("Hello, world!")
+        val ar = Array("Hello", "world")
+        val ae = new ArrayElement(ar)
+        val aewidth = ae.width
+        println(s"ae.width = $aewidth")
+        val er = ("aehoo")
+
+        val e: Element = new ArrayElement(Array(er))
+        val eheight = e.height
+        println(s"e.height = $eheight")
     }
 }
