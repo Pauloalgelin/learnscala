7ff55ee646453ca2da686eed0f8ce7f22872f6f0
Pauloalgelin <pauloalgelin@gmail.com>
2019-01-01 09:13:45 -0200

    Mixing in Traits extending a Class.'

---
+++ traits/Trait.scala
@@ -0,0 +1,17 @@
+trait Philosophical {
+    def philosophize = {
+        println("I consume memory, therefore I am!")
+    }
+}
+
+class Frog extends Philosophical {
+    override def toString = "green"
+}
+
+object run {
+    def main(args: Array[String]): Unit = {
+        val fo0 = new Frog
+        println(fo0)
+        fo0.philosophize
+    }
+}
