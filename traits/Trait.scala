8126c707c03e358ff6cf8f9f8a8a00a659a04cae
Pauloalgelin <pauloalgelin@gmail.com>
2019-01-01 12:05:38 -0200

    A Trait example.

--- traits/Trait.scala
+++ traits/Trait.scala
@@ -1,14 +1,33 @@
+class Point(val x: Int, val y: Int)
+
+trait Rectangular {
+    def topLeft: Point
+    def bottomRight: Point
+    def left = topLeft.x
+    def right = bottomRight.x
+    def width = right - left
+}
+
+abstract class Component extends Rectangular {
+    // more methods
+}
+
+class Rectangle(val topLeft: Point, val bottomRight: Point)
+    extends Rectangular {
+        // some methods
+    }
+
 class Animal
 
 trait Philosophical {
-    def philosophize = {
+    def philosophize() = {
         println("I consume memory, therefore I am!")
     }
 }
 
 class Frog extends Animal with Philosophical {
     override def toString = "green"
-    override def philosophize = {
+    override def philosophize() = {
         println(s"It ain't easy being $toString!")
     }
 }
@@ -22,5 +41,11 @@ object run {
         val fo1: Philosophical = new Frog
         println(fo1)
         fo1.philosophize
+
+        val pt0 = new Point(1, 1)
+        val pt1 = new Point(10, 10)
+        val rec = new Rectangle(pt0, pt1)
+        val recleft = rec.left
+        println(s"rec.left = $recleft")
     }
 }
